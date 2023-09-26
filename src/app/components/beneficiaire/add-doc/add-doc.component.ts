0ad53ae046db536e34b2c92a8322b1a0ebf81199
dutervil <dwadson3@gmail.com>
2023-09-26 08:36:27 -0400

    init

--- src/app/components/beneficiaire/add-doc/add-doc.component.ts
+++ src/app/components/beneficiaire/add-doc/add-doc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpEventType, HttpRequest} from "@angular/common/http";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-add-doc',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDocComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+  }
+
+  selectedFile: File | null = null;
+  uploadProgress = 0;
+  pdfPreviewUrl: SafeResourceUrl | null = null; // Use SafeResourceUrl
 
   ngOnInit(): void {
   }
 
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+
+    // Display the PDF preview when a PDF file is selected
+    if (this.selectedFile && this.selectedFile.type === 'application/pdf') {
+      this.readAndPreviewPdf();
+    } else {
+      this.pdfPreviewUrl = null;
+    }
+  }
+
+  uploadFile() {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+      formData.append('userId', 'your_user_id');
+      const req = new HttpRequest('POST', 'your_backend_url_here', formData, {
+        reportProgress: true // This enables progress tracking.
+      });
+
+      this.http.request(req).subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // @ts-ignore
+          this.uploadProgress = Math.round((100 * event.loaded) / event.total);
+        } else if (event.type === HttpEventType.Response) {
+
+          console.log('File upload complete:', event.body);
+        }
+      });
+    } else {
+      console.error('No file selected.');
+    }
+  }
+
+  readAndPreviewPdf() {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      // Sanitize and set the PDF data URL as a SafeResourceUrl
+      this.pdfPreviewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(e.target.result);
+    };
+
+    reader.onerror = (error) => {
+      console.error('Error reading the file:', error);
+    };
+
+    // @ts-ignore
+    reader.readAsDataURL(this.selectedFile);
+  }
 }
